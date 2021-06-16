427755b323def9e7a86f3d477d6327c7d6391dce
Tan-DESKPC <tan@tanho.ca>
2021-06-16 12:22:25 -0400

    update docs

--- R/utils.R
+++ R/utils.R
@@ -30,3 +30,11 @@ NULL
   httr::RETRY("GET", ...)
 }
 
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
